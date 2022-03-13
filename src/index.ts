7dfb31b22155bd4fb41bd3693a28a15467620c78
Kittisak Phormraksa <dnjooiopa@gmail.com>
2022-03-13 09:37:12 +0700

    Add hello message

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import { Client, Intents } from 'discord.js';
 
 import config from './config';
-console.log(config);
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
 client.on('ready', () => {
   console.log(`🟢 Client Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', (interaction) => {
+  const commands = interaction.channel.lastMessage.content.split(' ');
+  if (commands[0] === '!cdc') {
+    console.log('Time:', new Date());
+    console.log('Commands:', commands.join(' '));
+
+    if (commands.length === 1) {
+      interaction.channel.send('Hello from CDC Signal🚀');
+    }
+  }
 });
 
 client.login(config.CLIENT_TOKEN);
