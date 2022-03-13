b44d366184db5763ea0ae83681f78c3ee354f593
Kittisak Phormraksa <dnjooiopa@gmail.com>
2022-03-13 09:01:43 +0700

    Setup discord

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+import 'dotenv/config';
+
+const config = {
+  CLIENT_TOKEN: process.env.CLIENT_TOKEN,
+};
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello World');
+import { Client, Intents } from 'discord.js';
+
+import config from './config';
+console.log(config);
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on('ready', () => {
+  console.log(`🟢 Client Logged in as ${client.user.tag}`);
+});
+
+client.login(config.CLIENT_TOKEN);
