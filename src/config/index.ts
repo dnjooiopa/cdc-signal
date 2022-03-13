bde9d314153680c01a372e3f4b69e20bdf4405fb
Kittisak Phormraksa <dnjooiopa@gmail.com>
2022-03-13 21:48:28 +0700

    Add more envs

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,10 @@
 import 'dotenv/config';
 
 const config = {
-  CLIENT_TOKEN: process.env.CLIENT_TOKEN,
-  API_KEY: process.env.API_KEY,
+    CLIENT_TOKEN: process.env.CLIENT_TOKEN,
+    API_KEY: process.env.API_KEY,
+    UPDATE_TIME: process.env.UPDATE_TIME,
+    SIGNAL_CHANNEL: process.env.SIGNAL_CHANNEL,
 };
 
 export default config;
