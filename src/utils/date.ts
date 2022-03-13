c5460a214d350e794932c8e17780d5f8f045071a
Kittisak Phormraksa <dnjooiopa@gmail.com>
2022-03-13 19:56:17 +0700

    Fix localeDateString

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,10 +2,10 @@ export function getLocaleString(): string {
     return new Date().toLocaleString('en-US', { timeZone: 'Asia/Bangkok' });
 }
 
-export function getLocaleDate(time): string {
+export function getLocaleDate(): string {
     return new Date().toLocaleDateString('en-US', { timeZone: 'Asia/Bangkok' });
 }
 
-export function getDateFromTime(time: number) {
-    return new Date(time);
+export function getDateFromTime(time: number): string {
+    return new Date(time).toLocaleDateString('en-US', { timeZone: 'Asia/Bangkok' });
 }
