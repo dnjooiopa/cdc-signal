4fb3cd7796df3bf1d85fa3cc870a08c8bf708930
Kittisak Phormraksa <dnjooiopa@gmail.com>
2022-03-13 13:20:59 +0700

    Add getSignal

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,32 +1,43 @@
+import { SignalType } from './../model/index';
 import axios from 'axios';
 
 const TIME_IDX = 0;
 const CLOSING_PRICE_IDX = 4;
+const TIME_FRAME = '86400';
 
 export async function getOHLCs(url): Promise<number[]> {
-  try {
-    const response = await axios.get(url, {
-      params: {
-        after: (new Date().getTime() / 1000 - 86400 * 60).toFixed(0),
-        periods: '86400',
-      },
-    });
-    return response.data.result['86400'];
-  } catch (err) {
-    console.log('🔴 makeRequest error:', err.message);
-    return [];
-  }
+    try {
+        const options = {
+            params: {
+                after: (new Date().getTime() / 1000 - 86400 * 180).toFixed(0),
+                periods: TIME_FRAME,
+            },
+        };
+        const response = await axios.get(url, options);
+        return response.data.result[TIME_FRAME];
+    } catch (err) {
+        console.log('🔴 makeRequest error:', err.message);
+        return [];
+    }
 }
 
 export function calculateEMAs(prices: number[], length: number, smoothing = 2): number[] {
-  let EMAs = [];
-  const k = smoothing / (length + 1);
+    let EMAs = [];
+    const k = smoothing / (length + 1);
 
-  EMAs.push(prices.slice(0, length).reduce((total, current) => total + current, 0) / length);
-  for (const price of prices.slice(length)) {
-    EMAs.push(price * k + EMAs[EMAs.length - 1] * (1 - k));
-  }
-  return EMAs;
+    EMAs.push(prices.slice(0, length).reduce((total, current) => total + current, 0) / length);
+    for (const price of prices.slice(length)) {
+        EMAs.push(price * k + EMAs[EMAs.length - 1] * (1 - k));
+    }
+    return EMAs;
 }
 
-export function getSignal() {}
+export function getSignal(fastEma: number, fastEmaPrev: number, slowEma: number, slowEmaPrev: number): SignalType {
+    if (fastEma > slowEma && fastEmaPrev < slowEmaPrev) {
+        return 'buy';
+    } else if (fastEma < slowEma && fastEmaPrev > slowEmaPrev) {
+        return 'sell';
+    } else {
+        return 'none';
+    }
+}
--- tests/index.ts
+++ tests/index.ts
@@ -1,15 +1,28 @@
-import { calculateEMAs, getOHLCs } from 'src/utils';
+import { calculateEMAs, getOHLCs, getSignal } from '../src/utils';
 
 const TIME_IDX = 0;
 const CLOSING_PRICE_IDX = 4;
 
 (async () => {
-  console.log('🧪 makeRequest');
-  const OHLCs = await getOHLCs('https://api.cryptowat.ch/markets/binance/btcusdt/ohlc');
-  console.log(OHLCs[0]);
+    console.log('🧪 makeRequest');
+    const name = 'btc';
+    const pair = 'usdt';
 
-  console.log('🧪 calculateEMAs');
-  const closingPrices = OHLCs.map((o) => o[CLOSING_PRICE_IDX]);
-  const EMAs = calculateEMAs(closingPrices, 26);
-  console.log(EMAs);
+    const OHLCs = await getOHLCs(`https://api.cryptowat.ch/markets/binance/${name}${pair}/ohlc`);
+    console.log(OHLCs[0]);
+
+    console.log('🧪 calculateEMAs');
+    const closingPrices = OHLCs.map((o) => o[CLOSING_PRICE_IDX]);
+    const slowLength = 26;
+    const fastLength = 12;
+    const slowEMAs = calculateEMAs(closingPrices, slowLength);
+    const fastEMAs = calculateEMAs(closingPrices, fastLength).slice(slowLength - fastLength);
+    console.log(fastEMAs.slice(0, 10));
+
+    console.log('🧪 getSignal');
+    const times = OHLCs.map((o) => o[TIME_IDX] * 1000).slice(slowLength - 1);
+    for (let idx = 1; idx < times.length; idx++) {
+        const signal = getSignal(fastEMAs[idx], fastEMAs[idx - 1], slowEMAs[idx], slowEMAs[idx - 1]);
+        console.log(new Date(times[idx]), signal);
+    }
 })();
