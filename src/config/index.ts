4dee7256990ce0d0e69e225b1a737ebf2f2a8d79
Kittisak Phormraksa <dnjooiopa@gmail.com>
2022-03-14 13:03:30 +0700

    Add mqtt envs

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,6 +5,10 @@ const config = {
     API_KEY: process.env.API_KEY,
     UPDATE_TIME: process.env.UPDATE_TIME,
     SIGNAL_CHANNEL: process.env.SIGNAL_CHANNEL,
+    MQTT_HOST: process.env.MQTT_HOST,
+    MQTT_PORT: process.env.MQTT_PORT,
+    MQTT_USERNAME: process.env.MQTT_USERNAME,
+    MQTT_PASSWORD: process.env.MQTT_PASSWORD,
 };
 
 export default config;
