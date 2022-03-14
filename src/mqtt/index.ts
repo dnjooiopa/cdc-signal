33a711d7f4b4bdf23378f91ca5063728c089788e
Kittisak Phormraksa <dnjooiopa@gmail.com>
2022-03-14 13:03:53 +0700

    Add mqtt utils

---
+++ src/mqtt/index.ts
@@ -0,0 +1,21 @@
+import config from '../config';
+import mqtt from 'async-mqtt';
+import { getLocaleString } from 'src/utils/date';
+
+export async function publish(msg: string) {
+    try {
+        const client = await mqtt.connectAsync({
+            host: config.MQTT_HOST,
+            port: config.MQTT_PORT,
+            username: config.MQTT_USERNAME,
+            password: config.MQTT_PASSWORD,
+            protocol: 'mqtts',
+        });
+
+        await client.publish('cdc/signal', msg);
+        await client.end();
+        console.log(getLocaleString(), '🚀 Message published to mqtt host');
+    } catch (err) {
+        console.log(`🔴 MQTT error:`, err.message);
+    }
+}
