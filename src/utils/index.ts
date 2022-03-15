dcfbbbdabc643732eebd33d52a70d65669301df3
Kittisak Phormraksa <dnjooiopa@gmail.com>
2022-03-15 09:39:07 +0700

    Add cmd 'add'

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,10 @@ import config from '../config';
 import global from '../global';
 import { SignalObject, SignalType } from '../model/index';
 import { getDateFromTime, getLocaleString } from './date';
+import { saveFile } from './file';
 
+const TIME_IDX = 0;
+const CLOSING_PRICE_IDX = 4;
 const TIME_FRAME = '86400';
 
 export async function getOHLCs(exchange: string, symbol: string): Promise<any[]> {
@@ -23,6 +26,58 @@ export async function getOHLCs(exchange: string, symbol: string): Promise<any[]>
         console.log('🔴 makeRequest error:', err.message);
         return [];
     }
+}
+
+export async function update() {
+    for (const exchangeSymbol of global.state.exchangeSymbols) {
+        const { exchange, symbol } = exchangeSymbol;
+        console.log('---------------------------------------------');
+        console.log('Fetching', exchange, symbol);
+
+        const crypto = global.state.cryptos.find((c) => c.symbol === symbol);
+        if (crypto && crypto.times[crypto.times.length - 1] > new Date().getTime()) {
+            console.log('Already updated', exchange, symbol);
+            continue;
+        }
+
+        const ohlcs = await getOHLCs(exchange, symbol);
+        if (ohlcs.length < 30) {
+            console.log('Length too short or symbol not found', exchange, symbol);
+            continue;
+        }
+
+        let closingPrices = ohlcs.map((o) => o[CLOSING_PRICE_IDX]);
+
+        const slowLength = 26;
+        const fastLength = 12;
+        const slowEMAs = calculateEMAs(closingPrices, slowLength);
+        const fastEMAs = calculateEMAs(closingPrices, fastLength).slice(slowLength - fastLength);
+
+        closingPrices = closingPrices.slice(slowLength - 1);
+
+        const times = ohlcs.map((o) => o[TIME_IDX] * 1000).slice(slowLength - 1);
+
+        let signals: SignalType[] = ['none'];
+        for (let idx = 1; idx < times.length; idx++) {
+            const signal = getSignal(fastEMAs[idx], fastEMAs[idx - 1], slowEMAs[idx], slowEMAs[idx - 1]);
+            signals.push(signal);
+        }
+
+        global.state.cryptos = global.state.cryptos.filter((c) => c.symbol !== symbol);
+        global.state.cryptos.push({
+            symbol,
+            exchange,
+            times,
+            closingPrices,
+            fastEMAs,
+            slowEMAs,
+            signals,
+        });
+
+        console.log('Successfully fetched', exchange, symbol);
+    }
+    console.log('----------------------------------------------');
+    saveFile(global.DATA_PATH, JSON.stringify(global.state));
 }
 
 export function calculateEMAs(prices: number[], length: number, smoothing = 2): number[] {
@@ -87,4 +142,25 @@ export function getSignalObject(): SignalObject[] {
     }
 
     return signals;
+}
+
+export async function addSymbol(exchange: string, symbol: string): Promise<string> {
+    const existingSymbol = global.state.exchangeSymbols.find(
+        (es) => es.symbol === symbol.toLowerCase() && es.exchange === exchange.toLowerCase()
+    );
+    let msg = `\`${exchange.toUpperCase()}|${symbol.toUpperCase()}\` is already existed.`;
+    if (!existingSymbol) {
+        const ohlcs = await getOHLCs(exchange.toLowerCase(), symbol.toLowerCase());
+        if (ohlcs.length < 30) {
+            msg = `🔴 Length too short or symbol not found: \`${exchange.toUpperCase()}|${symbol.toUpperCase()}\``;
+        } else {
+            global.state.exchangeSymbols.push({
+                exchange,
+                symbol,
+            });
+            await update();
+            msg = `symbol \`${exchange.toUpperCase()}|${symbol.toUpperCase()}\` has been added.`;
+        }
+    }
+    return msg;
 }
